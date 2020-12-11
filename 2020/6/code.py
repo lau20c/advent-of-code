0e756c514fae24b0ae75943170ee313b156e4d7a
lau20c <46605726+lau20c@users.noreply.github.com>
2020-12-10 22:28:29 -0500

    starting day 6

---
+++ 2020/6/code.py
@@ -0,0 +1,14 @@
+import os
+
+with open(os.path.join(os.path.dirname(__file__), "input.txt"), "r") as f:
+    data = [list(line.strip()) for line in f]
+
+
+# def partOne():
+
+
+# def partTwo():
+
+
+# print(partOne(data))
+# print(partTwo(data))
